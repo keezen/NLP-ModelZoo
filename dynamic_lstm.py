52fb03ef5f77e1f1c60217b8342a25f173d97dc9
Zhen Ke <keezen@qq.com>
2019-03-05 10:55:59 +0800

    dynamic RNN wrapper

--- dynamic_lstm.py
+++ dynamic_lstm.py
@@ -60,3 +60,46 @@ class DynamicLSTM(nn.Module):
         y = torch.index_select(y_sort, dim=0, index=idx_unsort)
 
         return y
+
+
+class DynamicRnnWrapper(nn.Module):
+    """
+    Dynamic RNN wrapper.
+
+    Inputs
+    ------
+    input: tensor, shaped [batch, max_step, input_size]
+    seq_lens: tensor, shaped [batch], sequence lengths of batch
+
+    Outputs
+    -------
+    output: tensor, shaped [batch, max_step, num_directions * hidden_size],
+         tensor containing the output features (h_t) from the last layer
+         of the LSTM, for each t.
+    """
+
+    def __init__(self, rnn_module):
+        super(DynamicRnnWrapper, self).__init__()
+        self.rnn_module = rnn_module
+
+    def forward(self, x, seq_lens):
+        # sort input by descending length
+        _, idx_sort = torch.sort(seq_lens, dim=0, descending=True)
+        _, idx_unsort = torch.sort(idx_sort, dim=0)
+        x_sort = torch.index_select(x, dim=0, index=idx_sort)
+        seq_lens_sort = torch.index_select(seq_lens, dim=0, index=idx_sort)
+
+        # pack input
+        x_packed = pack_padded_sequence(
+            x_sort, seq_lens_sort, batch_first=True)
+
+        # pass through rnn
+        y_packed, _ = self.rnn_module(x_packed)
+
+        # unpack output
+        y_sort, length = pad_packed_sequence(y_packed, batch_first=True)
+
+        # unsort output to original order
+        y = torch.index_select(y_sort, dim=0, index=idx_unsort)
+
+        return y
