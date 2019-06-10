aa7d280519cc1a0d1f2b69f8931168ef7d2d8851
Zhen Ke <keezen@qq.com>
2019-06-10 15:17:11 +0800

    save json

--- utils.py
+++ utils.py
@@ -226,3 +226,15 @@ def flatten_list(nested_list):
     else:
         for item in nested_list:
             yield from flatten_list(item)
+
+            
+def read_json(json_path):
+    """Read json."""
+    with open(json_path, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+
+def save_json(obj, json_path):
+    """Save json."""
+    with open(json_path, 'w', encoding='utf-8') as f:
+        json.dump(obj, f, ensure_ascii=False, indent=4)
